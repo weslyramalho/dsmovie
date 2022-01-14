e2ce4d080648634c80fa52ac88a3f3c514f61207
Wesly <weslyramalho@yahoo.com.br>
2022-01-14 20:42:34 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,15 +1,18 @@
 import MovieStar from "components/MovieStar";
 import "./styled.css";
 
+type Props = {
+    score: number;
+    count: number;
+}
 
-function MovieScore(){
+function MovieScore({score, count} : Props){
 
-    const score = 3.5;
-    const count = 13;
+    
     return(
         <div className="dsmovie-score-container">
          <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-         <MovieStar />
+         <MovieStar score={score}/>
         <p className="dsmovie-score-count">{count} avaliações</p>
 </div>
     )
